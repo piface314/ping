8b2a9a4fa2d8a81fb5f6605610eb33461fa2a753
Henrique Santana <henriquessantana314@gmail.com>
2019-03-25 23:28:54 -0300

    Commit inicial

---
+++ src/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.title = 'Ping'
+    t.window.width = 800
+    t.window.height = 600
+    t.modules.physics = false
+    t.console = true
+    t.window.resizable = false
+    t.window.fullscreen = false
+end
